2ba2eaf4549f00b126790ced38c81b9290772f2a
Alex <alex7breis@gmail.com>
2019-09-22 15:46:12 -0300

    Inicio Tela Cadastro

---
+++ app/src/main/java/com/example/done_app_mvp/cadastro/Cadastro.java
@@ -0,0 +1,15 @@
+package com.example.done_app_mvp.cadastro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.done_app_mvp.R;
+
+public class Cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
